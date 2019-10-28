45c354d0bfe4b8ac6834c08538ca4c9f877109b9
Gianni <gianpietro.signorini@nhs.net>
2019-10-28 16:40:07 +0000

    character count array

---
+++ countDigit.c
@@ -0,0 +1,37 @@
+/* count digits, white space, others */
+
+#include <stdio.h>
+
+int main(void)
+{
+  int stp;
+  int c, i, nwhite, nother;
+  int ndigit[10];
+
+  nwhite = nother = 0;
+ 
+   for (i = 0; i < 10; ++i)
+     ndigit[i] = 0;
+
+  while ((c = getchar()) != EOF) 		// EOF in Windows ** ctrl + Z + Enter **     in Linux ** ctrl + D **
+    if (c >= '0' && c <= '9')
+         ++ndigit[c-'0']; 	 		// '0' has a value of 48 so this sets values from 0 to 9 and adds 1 to array index value
+  // ndigit[c-'0'] = ndigit[c-'0'] + 1;         // this line is the full expression of the line above and so gives same result
+    else if (c == ' ' || c == '\n' || c == '\t')
+      ++nwhite;
+    else
+      ++nother;
+
+
+  printf("digits =");
+
+  for (i = 0; i < 10; ++i)
+    printf(" %d", ndigit[i]);
+
+  printf(", white space = %d, other = %d\n",nwhite, nother);
+
+  scanf("%d", &stp);
+
+  return 0;
+
+}
