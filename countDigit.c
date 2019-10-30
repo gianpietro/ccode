40c16f62c052b19e80f9c7ba205e1ef341e1a625
Gianni <gianpietro.signorini@nhs.net>
2019-10-30 10:54:30 +0000

    horizontal histogram graph

--- countDigit.c
+++ countDigit.c
@@ -7,95 +7,88 @@ int main(void)
   int stp;
   int c, i, nwhite, nother;
   int ndigit[10];
-  int j, k, m;
-  int x, y, z;
-  int p, q, r, w;
-  m = 0;
-  j = 0;
+  int r, x, y, z, m, k;
   
-
+  i = k = r = x = y = z = m = 0;
   nwhite = nother = 0;
+  
+  #define UPPER 10		// upper limit of array  
  
-   for (i = 0; i < 10; ++i)
+  for (i = 0; i < UPPER; ++i)
      ndigit[i] = 0;
 
-  while ((c = getchar()) != EOF) 		// EOF in Windows ** ctrl + Z + Enter **     in Linux ** ctrl + D **
+  /* following code allows for user to enter a series of digits
+     and characters until the EOF is reached. 
+     To get EOF in Windows press ctrl+Z for Linux press ctrl+D 
+  */ 
+  while ((c = getchar()) != EOF) 		
     if (c >= '0' && c <= '9')
-         ++ndigit[c-'0']; 	 		// '0' has a value of 48 so this sets values from 0 to 9 and adds 1 to array index value
-  // ndigit[c-'0'] = ndigit[c-'0'] + 1;         // this line is the full expression of the line above and so gives same result
-    else if (c == ' ' || c == '\n' || c == '\t')
-      ++nwhite;
+         ++ndigit[c-'0']; 	 	            // '0' has a value of 48. Sets c = 0 to 9. Index array + 1
+  /* ndigit[c-'0'] = ndigit[c-'0'] + 1; */          //  same achieved by
+    else if (c == ' ' || c == '\n' || c == '\t')    
+      ++nwhite;				            //  count white spaces
     else
-      ++nother;
-
+      ++nother;                                     //  count any other tyoe of char
 
   printf("digits =");
-
-  for (i = 0; i < 10; ++i)
+  for (i = 0; i < UPPER; ++i)
     printf(" %d", ndigit[i]);
-
-  printf(", white space = %d, other = %d\n",nwhite, nother);
+  printf(", white space = %d, other = %d\n",nwhite, nother);     // totals for each char digit, white space and other
  
-  /* histogram with verical bars*/
-    for (j=0; j<10; j++){
-      k = ndigit[j];
-      for (m=0; m <k; m++){ 
+  printf("\n");
+  printf("Vertical Horizontal histogram of digit values between 0 and 9\n");
+  printf("------------------------------------------------------\n");
+  printf("\n");  
+  for (i=0; i < UPPER; i++){   
+      k = ndigit[i];
+      for (m=0; m < k; m++){ 
          printf("*");
       }   
       printf("\n");     
     }
   
-    /* Find the array index with the largest value
-       then start a loop from that value and go through 
-       array, if the array index is that value print else
-       move once space along and check next array value.
-       When have completed loop start loop again with the start
-       value at largest array value less 1
-    */
-
-
-
-    
-    y = 0;
-    z = 0;
-    r =0;
-    w = 0;
-
-    for (x=0; x<10; x++){
-       y = ndigit[x];
+    printf("\n");
+  
+    /* find the highest value held in the array */
+    for (i=0; i < UPPER; i++){   
+       y = ndigit[i];
        if (y > z) {
-         z = y;
+         z = y;                 // z will hold the highest value
        }
     }
+    printf("digits =");
+    for (i = 0; i < UPPER; ++i)
+      printf(" %d", ndigit[i]);
+    printf(", white space = %d, other = %d\n",nwhite, nother);   
     printf("value of largest item %d\n", z);
+    printf("\n");
+    printf("Horizontal histogram of digit values between 0 and 9\n");
+    printf("----------------------------------------------------------------------------\n");
+    printf("\n");   
+    for (i = 0; i < UPPER; ++i)
+       printf("%d\t", i);
+    printf("\n----------------------------------------------------------------------------\n");
+    printf("\n");
 
-    w = z;
-    for (r=z; r>0; r--){
-     for(q=0; q<10; q++){
-       
-   	if(ndigit[q] == z){  
-           printf("*");
-	   // w--;
-	} else if (ndigit[q] == w){
-	   printf("*");
-	} else {
-	  printf("--");
-	}
-    
-     }
-     
-     printf("\n");
-    }
-   
-    
+    /* The digit which occurs the most is stored in z
+       using a loop from z down to zero if the array index
+       has a value of z or higher then it will print an astrix.
+       The value of z is then reduced by 1. Again the loop starts and
+       if the value in the array index is higher or equale z an
+       astrix will be printed */
 
-    
-
-
-       
+    for (r=z; r > 0; r--){         
+      for (i=0; i < UPPER; i++){  
+   	if(ndigit[i] >= z)
+          printf("*\t");
+	else
+	  printf(" \t");
+      }
+       printf("\n");
+      z--;
+    }          
  
   scanf("%d", &stp);
 
   return 0;
-
 }
