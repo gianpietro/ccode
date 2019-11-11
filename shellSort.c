82b0af217d00ab243834c1cec2cfc2a48b90b4b1
Gianpietro Signorini <gianpietros@aol.com>
2019-11-11 22:41:28 +0000

    Shell sort

--- shellSort.c
+++ shellSort.c
@@ -1,26 +1,30 @@
 /*shell sort */
 #include <stdio.h>
 
+#define ANSI_COLOR_RESET "\x1b[0m"
+
 void shellsort(int *, int);
 
 int main(void) {
 
-  int i;
-  int y;
-  y=6;
+  int i, k;
+  int iarray;
   
-  int array[] = {3,5,7,8,9,1};
+  int array[] = {3,5,7,8,9,11,1,4};
+  iarray = sizeof(array)/sizeof(int);
+  printf("size of array %d\n", iarray);
 
-  for (i=0; i<y; ++i){
+  for (i=0; i<iarray; ++i){
     printf("%d ", array[i]);
   }
 
-  shellsort(array, y);
+  shellsort(array, iarray);
   printf("\n");
   
-  for (i=0; i<y; ++i){
+  for (i=0; i<iarray; ++i){
     printf("%d ", array[i]);
   }
+  printf("\n");
 
   return 0;
 }
@@ -28,12 +32,18 @@ int main(void) {
 void shellsort(int v[], int n){
   printf("\n");
   int gap, i, j, temp;
+  int m;
+  
   printf("%d\n",n);
+
   for (gap = n/2; gap > 0; gap /=2){
+    printf("--------------------------------\n");
     printf("gap outer loop %d\n", gap);
+    printf("--------------------------------\n");
     for (i=gap; i < n; i++){
       printf("i middle loop %d\n", i);
       printf("gap middle loop %d\n", gap);
+      printf("\n");
       for (j=i-gap; j>=0 && v[j]>v[j+gap]; j-=gap){
 	printf("i inner loop %d\n", i);
 	printf("j inner loop %d\n", j);
@@ -45,8 +55,16 @@ void shellsort(int v[], int n){
         v[j] = v[j+gap];
         printf("v[j]=v[j+gap]inner loop %d\n", v[j]);
         v[j+gap] = temp;
+	for (m=0; m<n; ++m){
+	  printf("\033[5;32m %d ", v[m]);
+	  
+         }
+	printf(ANSI_COLOR_RESET);
    }
+       
+       printf("\n");
     }
+   
   }
   
 }
