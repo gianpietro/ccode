e4e51698514baf3b1d34ee965c63f14230cd7464
Gianpietro Signorini <gianpietros@aol.com>
2019-11-14 23:00:05 +0000

    Calculator with push and pop

--- calculator.c
+++ calculator.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define MAXOP 100
 #define NUMBER '0'
-#define MAXVAL 100
 
-int getop (char[]);
+#define MAXVAL 100
+#define BUFSIZE 100
+
+int getop (char []);
 void push(double);
 double pop(void);
+
 int sp = 0;
 double val[MAXVAL];
+
+int getch(void);
+void ungetch(int);
+
+char buf[BUFSIZE];
+int bufp = 0;
 
 int main(void){
   int type;
@@ -24,6 +34,9 @@ int main(void){
     case '+':
       push(pop() + pop());
       break;
+    case '*':
+      push(pop() * pop());
+      break;
     case '-':
       op2 = pop();
       push(pop() - op2);
@@ -36,14 +49,13 @@ int main(void){
         printf("error: zero divisor\n");
       break;
     case '\n':
-      printf("t%.8g\n", pop());
+      printf("\t%.8g\n", pop());
       break;
     default:
       printf("error: unknow command %s\n", s);
       break;
     }
   }
-  
   return 0;
 }
 
@@ -61,4 +73,49 @@ double pop(void){
     printf("error: stack empty\n");
   return 0.0;
 }
+
+int getop(char s[]){
+  int i, c;
+
+  while ((s[0] = c = getch()) == ' ' || c == '\t')
+    ;
+  s[1] = '\0';
+  if(!isdigit(c) && c != '.')
+    return c;
+  i = 0;
+  if (isdigit(c))
+    while (isdigit(s[++i] = c = getch()))
+      ;
+  if (c == '.')
+    while (isdigit(s[++i] = c = getch()))
+      ;
+  s[i] = '\0';
+  if (c != EOF)
+    ungetch(c);
+        
+  return NUMBER;
+}
+
+int getch(void){
+   return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c){
+  if (bufp >= BUFSIZE)
+    printf("ungetch: too many characters\n");
+  else
+    buf[bufp++] = c;
+}
+  
+
+
+
+
+
+
+
+
+
+
+
 
